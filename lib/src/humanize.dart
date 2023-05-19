008264c72ff5cfff469b215374b27ba60bb563e0
Olivier Roudaut <34065489+oraworaw@users.noreply.github.com>
2023-05-19 13:19:59 +1000

    update dart

--- lib/src/humanize.dart
+++ lib/src/humanize.dart
@@ -1,8 +1,6 @@
 /// Convert an integer to its ordinal as a string. 1 is '1st', 2 is '2nd',
 /// 3 is '3rd', etc. works for any integer.
 String ordinal(int value) {
-  assert(value != null, '[value] must not be null');
-
   int tempValue;
   dynamic templates;
   String finalValue;
@@ -54,38 +52,37 @@ String ordinal(int value) {
   return finalValue;
 }
 
-
 /// Convert an integer to a string containing commas every three digits.
 /// For example, 3000 becomes '3,000' and 45000 becomes '45,000'.
 String intComma(int value) {
-   // Convert int value to a String one
-   String valueToString = '$value';
+  // Convert int value to a String one
+  String valueToString = '$value';
 
-   // Use RegExp function witch used to match strings or parts of strings
-   RegExp reg = new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))');
-   Function mathFunc = (Match match) => '${match[1]},';
-   
-   // Store the result in a new String value
+  // Use RegExp function witch used to match strings or parts of strings
+  RegExp reg = new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))');
+  final mathFunc = (Match match) => '${match[1]},';
 
-   String newValue = valueToString.replaceAllMapped(reg, mathFunc);
+  // Store the result in a new String value
 
-   return '$newValue';
+  String newValue = valueToString.replaceAllMapped(reg, mathFunc);
+
+  return '$newValue';
 }
 
 /// Convert a large integer to a friendly text representation. Works best
 /// for numbers over 1 million. For example, 1000000 becomes '1.0 million',
 /// 1200000 becomes '1.2 million' and '1200000000' becomes '1.2 billion'.
 String intWord(int value) {
-  if (value < 1000000){
-    return "$value";
+  if (value < 1000000) {
+    return '$value';
   }
   // TODO : Implement numbers Hight to 1M
+  return '';
 }
 
 /// For numbers 1-9, return the number spelled out. Otherwise, return the
 /// number. This follows Associated Press style.
 String appNumber(int value) {
-  
   List values = [
     "one",
     "two",
@@ -100,8 +97,8 @@ String appNumber(int value) {
 
   if (value < 0 || value > 10) {
     return "$value";
-  }else{
-      return values[value-1];
+  } else {
+    return values[value - 1];
   }
 }
 
@@ -117,4 +114,3 @@ String naturalDay(DateTime value) {
 String naturalTime(DateTime value) {
   return "";
 }
- 
